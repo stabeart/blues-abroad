de6bcff50e91122c69348a73b082205213e6a7dc
YourGitHubUsername <your_github_email@example.com>
2025-07-10 15:07:16 -0400

    killing myself

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 import HomePage from './pages/HomePage';
 import PostPage from './pages/PostPage';
 
